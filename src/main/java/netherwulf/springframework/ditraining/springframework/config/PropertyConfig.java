921ccc1627bcd58d578e9d4bd151452e8735eca5
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-07-25 13:24:56 +0200

    Packages clean-up
    Packages clean-up

--- src/main/java/netherwulf/springframework/ditraining/springframework/config/PropertyConfig.java
+++ src/main/java/netherwulf/springframework/ditraining/springframework/config/PropertyConfig.java
@@ -1,4 +1,60 @@
 package netherwulf.springframework.ditraining.springframework.config;
 
+import netherwulf.springframework.ditraining.springframework.examplebeans.FakeDataSource;
+import netherwulf.springframework.ditraining.springframework.examplebeans.FakeJmsBroker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
+@Configuration
+@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
+
+    @Value("${netherwulf.username}")
+    String username;
+
+    @Value("${netherwulf.password}")
+    String password;
+
+    @Value("${netherwulf.dburl}")
+    String url;
+
+    @Value("${netherwulf.jms.username}")
+    String jmsUsername;
+
+    @Value("${netherwulf.jms.password}")
+    String jmsPassword;
+
+    @Value("${netherwulf.jms.url}")
+    String jmsUrl;
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(env.getProperty("NETHERWULF_USERNAME"));
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker(){
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUsername(jmsUsername);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUrl);
+        return fakeJmsBroker;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties(){
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 }
--- src/main/java/netherwulf/springframework/ditraining/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/netherwulf/springframework/ditraining/springframework/controllers/PropertyInjectedController.java
@@ -1,7 +1,6 @@
-package netherwulf.springframework.ditraining.controllers;
+package netherwulf.springframework.ditraining.springframework.controllers;
 
-import netherwulf.springframework.ditraining.services.GreetingService;
-import netherwulf.springframework.ditraining.services.GreetingServiceImpl;
+import netherwulf.springframework.ditraining.springframework.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
